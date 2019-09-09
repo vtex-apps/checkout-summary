822847232cb1231b46d9306d8edb329125189fb6
Jefferson <jefiim.ad@gmail.com>
2019-09-09 15:30:46 -0300

    Small fix to Tax message id

--- react/components/SummaryItem.tsx
+++ react/components/SummaryItem.tsx
@@ -24,7 +24,7 @@ defineMessages({
     id: 'store/checkout-summary.Discounts',
     defaultMessage: 'Discounts',
   },
-  Taxa: {
+  Tax: {
     id: 'store/checkout-summary.Tax',
     defaultMessage: 'Taxes',
   },
