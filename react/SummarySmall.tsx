834a82adb36b05fecd418fc926452db53527dfa8
victorhmp <victor2142@gmail.com>
2019-11-05 21:46:06 -0300

    Create SummarySmall component and interface

---
+++ react/SummarySmall.tsx
@@ -0,0 +1,32 @@
+import React, { FunctionComponent } from 'react'
+import { FormattedMessage } from 'react-intl'
+
+import SummaryContextProvider from './SummaryContext'
+
+interface Props {
+  totalizers: Totalizer[]
+  total: number
+  totalizersToShow: string[]
+}
+
+const SummarySmall: FunctionComponent<Props> = ({
+  total,
+  children,
+  totalizers,
+  totalizersToShow = ['Items'],
+}) => {
+  const filteredTotalizers = totalizers.filter(totalizer =>
+    totalizersToShow.includes(totalizer.id)
+  )
+
+  return (
+    <SummaryContextProvider totalizers={filteredTotalizers} total={total}>
+      <div className="c-on-base">{children}</div>
+      <span className="t-small db mv4">
+        <FormattedMessage id="store/checkout-summary.disclaimer" />
+      </span>
+    </SummaryContextProvider>
+  )
+}
+
+export default SummarySmall
